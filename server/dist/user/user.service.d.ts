4167cbaf1ea4b1fd67e68a157b2a6285cb950c0d
pulipur10 <mindy040320@gmail.com>
2025-09-23 19:48:12 +0900

    Delete server directory

--- server/dist/user/user.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UserService {
-    findAll(): {
-        id: number;
-        email: string;
-        nickname: string;
-    }[];
-}
--- server/src/user/user.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UserService {
-  findAll() {
-    return [{ id: 1, email: 'test@ewha.ac.kr', nickname: 'nickname' }];
-  }
-}
