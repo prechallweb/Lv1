7848d7bce5c3205ad08f5cb24774190dee8b8cb4
aroro6210 <aroro6210@ewha.ac.kr>
2025-09-29 21:12:08 +0900

    백

---
+++ back/src/main/java/com/hwawon/ewhaian/EwhaianApplication.java
@@ -0,0 +1,13 @@
+package com.hwawon.ewhaian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwhaianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwhaianApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/hwawon/ewhaian/EwhaianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwawon.ewhaian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhaianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
